942d22c2dafaf77c36b19503f317343f78ce75d7
yuki <38411009+yukijina@users.noreply.github.com>
2026-03-11 06:05:32 -0700

    dispatch fetched data and error handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        quastions: action.payload,
+        status: 'ready',
+      };
+
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error('Error'));
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
