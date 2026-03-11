ec9dd8506c661cde783f0df5120e73ae3baf8377
yuki <38411009+yukijina@users.noreply.github.com>
2026-03-11 06:24:12 -0700

    display loading, ready and startScreen components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
 
 const initialState = {
   questions: [],
@@ -14,7 +17,7 @@ function reducer(state, action) {
     case 'dataReceived':
       return {
         ...state,
-        quastions: action.payload,
+        questions: action.payload,
         status: 'ready',
       };
 
@@ -29,7 +32,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // destrictire state as {questions, status}
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
@@ -42,8 +48,9 @@ function App() {
     <div className='app'>
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
